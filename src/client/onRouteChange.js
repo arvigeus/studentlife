80138f8ecb8baee803393f84bb6a88200a217f9c
Nikolay Stoynov <arvigeus@debian.localhost>
2016-11-08 16:33:02 +0200

    Add TODO

--- src/client/onRouteChange.js
+++ src/client/onRouteChange.js
@@ -2,6 +2,8 @@ require('smoothscroll-polyfill').polyfill();
 
 // if (window.ga) window.ga('send', 'pageview');
 
+// TODO: Scroll fires after initial render and causes wrong scrolling
+
 export default (prevState, nextState, replace) => {
   window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   if (window.ga) {
